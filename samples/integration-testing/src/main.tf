7aa946e4dec88822253ed2401c1c544cc2f8bda9
tperry-cloudreach <50703436+tperry-cloudreach@users.noreply.github.com>
2021-08-15 15:09:47 +0100

    Add environment tags to RG

--- samples/integration-testing/src/main.tf
+++ samples/integration-testing/src/main.tf
@@ -10,4 +10,7 @@ resource "azurerm_virtual_network" "vnet" {
     address_space       = ["10.0.0.0/16"]
     location            = var.location
     resource_group_name = azurerm_resource_group.rg.name
+    tags = {
+      "environment" = "devtest"
+    }
 }
