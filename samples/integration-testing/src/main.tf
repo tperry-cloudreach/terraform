4e050c7d271abeeac9ad175170e781b773ff0832
tperry-cloudreach <50703436+tperry-cloudreach@users.noreply.github.com>
2021-08-15 14:38:09 +0100

    Deploy VNET into RG

--- samples/integration-testing/src/main.tf
+++ samples/integration-testing/src/main.tf
@@ -4,3 +4,10 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-hello-tf-${random_uuid.uuid.result}"
   location = var.location
 }
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name                = "tf-vnet-${random_uuid.uuid.result}"
+    address_space       = ["10.0.0.0/16"]
+    location            = var.location
+    resource_group_name = azurerm_resource_group.rg.name
+}
