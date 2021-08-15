da7f660760a04bff18cfa66fb47e1ebd7bdebd84
tperry-cloudreach <50703436+tperry-cloudreach@users.noreply.github.com>
2021-08-15 15:29:39 +0100

    Pipeline updates

--- samples/integration-testing/src/main.tf
+++ samples/integration-testing/src/main.tf
@@ -1,12 +1,12 @@
 resource "random_uuid" "uuid" {}
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-hello-tf-${random_uuid.uuid.result}"
+  name     = "rg-hello-tf"
   location = var.location
 }
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
-    name                = "tf-vnet-${random_uuid.uuid.result}"
+    name                = "tf-vnet-01"
     address_space       = ["10.0.0.0/16"]
     location            = var.location
     resource_group_name = azurerm_resource_group.rg.name
