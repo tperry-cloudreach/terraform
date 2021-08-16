dbf5916dd9834bd169217fe605b99d719b9b7551
tperry-cloudreach <50703436+tperry-cloudreach@users.noreply.github.com>
2021-08-16 09:25:51 +0100

    Move to remote state

--- samples/integration-testing/src/main.tf
+++ samples/integration-testing/src/main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.72.0"
+    }
+  }
+    backend "azurerm" {
+        resource_group_name  = "tfstate"
+        storage_account_name = "tfstate4875"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+
+}
+
 resource "random_uuid" "uuid" {}
 
 resource "azurerm_resource_group" "rg" {
