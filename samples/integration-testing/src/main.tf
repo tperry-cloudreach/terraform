ac4c55a3f4a7df8f6a7bd824748e806a10e48354
tperry-cloudreach <50703436+tperry-cloudreach@users.noreply.github.com>
2021-09-02 10:03:54 +0100

    updated tags and created new draft domain controller module

--- samples/integration-testing/src/main.tf
+++ samples/integration-testing/src/main.tf
@@ -24,7 +24,5 @@ resource "azurerm_virtual_network" "vnet" {
     address_space       = ["10.0.0.0/16"]
     location            = var.location
     resource_group_name = azurerm_resource_group.rg.name
-    tags = {
-      "environment" = "devtest"
-    }
+    tags = var.tags
 }
--- samples/integration-testing/src/variables.tf
+++ samples/integration-testing/src/variables.tf
@@ -3,3 +3,8 @@ variable location {
   description = "The Azure location where the resources will be created"
   default     = "uksouth"
 }
+variable "tags" {
+      business_owner = "placeholder"
+      cost_centre = "placeholder"
+      application_name = "placeholder"
+}
