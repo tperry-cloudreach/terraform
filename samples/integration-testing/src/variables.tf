112247998f83a182ade65997db363869a70a24b1
tperry-cloudreach <50703436+tperry-cloudreach@users.noreply.github.com>
2021-09-02 10:18:14 +0100

    removed tags in variables

--- samples/integration-testing/src/variables.tf
+++ samples/integration-testing/src/variables.tf
@@ -3,8 +3,3 @@ variable location {
   description = "The Azure location where the resources will be created"
   default     = "uksouth"
 }
-variable "tags" {
-      business_owner = "placeholder"
-      cost_centre = "placeholder"
-      application_name = "placeholder"
-}
