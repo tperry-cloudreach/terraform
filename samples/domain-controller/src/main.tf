d355d4731580b49c218639622ce69db703c67bde
tperry-cloudreach <50703436+tperry-cloudreach@users.noreply.github.com>
2021-09-02 10:52:00 +0100

    new pipeline for DC

---
+++ samples/domain-controller/src/main.tf
@@ -0,0 +1,128 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.72.0"
+    }
+  }
+    backend "azurerm" {
+        resource_group_name  = "tfstate"
+        storage_account_name = "tfstate4875"
+        container_name       = "tfstate"
+        key                  = "adds.terraform.tfstate"
+    }
+
+}
+
+# Create a resource group if it doesn't exist
+resource "azurerm_resource_group" "myterraformgroup" {
+    name     = local.apps_resource_group_name
+    location = local.region
+
+    tags = local.tags
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "myVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = local.region
+    resource_group_name = local.apps_resource_group_name
+
+    tags = local.tags
+}
+
+# Create subnet
+resource "azurerm_subnet" "myterraformsubnet" {
+    name                 = "mySubnet"
+    resource_group_name  = local.apps_resource_group_name
+    virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
+    address_prefixes       = ["10.0.1.0/24"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "myterraformpublicip" {
+    name                         = "myPublicIP"
+    location                     = local.region
+    resource_group_name          = local.apps_resource_group_name
+    allocation_method            = "Dynamic"
+
+    tags = local.tags
+    }
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "myterraformnsg" {
+    name                = "myNetworkSecurityGroup"
+    location            = local.region
+    resource_group_name = local.apps_resource_group_name
+
+    security_rule {
+        name                       = "RDP"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "3389"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = local.tags
+}
+
+# Create network interface
+resource "azurerm_network_interface" "myterraformnic" {
+    name                      = "myNIC"
+    location                  = local.region
+    resource_group_name       = local.apps_resource_group_name
+
+    ip_configuration {
+        name                          = "myNicConfiguration"
+        subnet_id                     = azurerm_subnet.myterraformsubnet.id
+        private_ip_address_allocation = "Dynamic"
+        public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
+    }
+
+    tags = local.tags
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "nicnsgassociation" {
+    network_interface_id      = azurerm_network_interface.myterraformnic.id
+    network_security_group_id = azurerm_network_security_group.myterraformnsg.id
+}
+
+# Generate random text for a unique storage account name
+resource "random_id" "randomId" {
+    keepers = {
+        # Generate a new ID only when a new resource group is defined
+        resource_group = local.apps_resource_group_name
+    }
+
+    byte_length = 8
+}
+# Create virtual machine
+resource "azurerm_windows_virtual_machine" "domaincontroller" {
+  name                = "adds-dc-01"
+  resource_group_name = local.apps_resource_group_name
+  location            = local.region
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.myterraformnic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
